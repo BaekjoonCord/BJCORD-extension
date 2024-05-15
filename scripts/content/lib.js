0c1a5e5079250fc3329d58160589901838afb171
YEAHx4 <developerstarlight@gmail.com>
2024-05-15 22:08:07 +0900

    comment

--- scripts/content/lib.js
+++ scripts/content/lib.js
@@ -411,6 +411,12 @@ async function getProblemData(id) {
   });
 }
 
+/**
+ * 크롬 스토리지에 저장된 웹훅 목록을 불러옵니다.
+ * 각 웹훅 객체는 `{name, url, enabled}`로 구성됩니다.
+ *
+ * @returns {Array<Object>} 웹훅 목록을 반환합니다.
+ */
 async function getWebhooks() {
   const data = await chrome.storage.sync.get("webhooks");
   return data.webhooks || [];
