e493e41061c543697737fe3cd1af035b75940b35
YEAHx4 <developerstarlight@gmail.com>
2025-09-23 15:36:06 +0900

    fix: 텍스트끼리 너무 붙어있던 문제 해결, 비율이 안맞던 문제 해결

--- components/options/option-webhook-item.tsx
+++ components/options/option-webhook-item.tsx
@@ -5,17 +5,23 @@ export default function OptionWebhookItem({ webhook }: { webhook: Webhook }) {
   return (
     <div
       className={cn(
-        "flex justify-between items-center w-full gap-2",
+        "flex justify-between items-center w-full",
         "h-8 bg-[#2c2f33] pl-4 pr-2 rounded-sm min-w-0"
       )}
     >
-      <div className={cn("w-[20%] hover:cursor-text h-full flex items-center")}>
+      <div
+        className={cn("w-[20%] hover:cursor-text h-full flex items-center p-1")}
+      >
         <span className="w-full truncate">{webhook.name}</span>
       </div>
-      <div className={cn("w-64 hover:cursor-text h-full flex items-center")}>
+      <div
+        className={cn("w-[50%] hover:cursor-text h-full flex items-center p-1")}
+      >
         <span className="w-full truncate">{webhook.url}</span>
       </div>
-      <div className={cn("w-[25%] hover:cursor-text h-full flex items-center")}>
+      <div
+        className={cn("w-[25%] hover:cursor-text h-full flex items-center p-1")}
+      >
         <span className="w-full truncate">{webhook.displayName}</span>
       </div>
       <div
