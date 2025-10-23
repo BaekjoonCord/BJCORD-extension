b439973116d065673dc843ad43f0537d0da46ebd
YEAHx4 <developerstarlight@gmail.com>
2025-10-23 13:28:37 +0900

    doc: logger.ts 주석 추가

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,13 +1,30 @@
 import { isDev } from "./util";
 
+/**
+ * 로그를 출력합니다. 브라우저의 콘솔에 출력됩니다.
+ * 개발 모드인 경우 `[DEV]` 태그가 붙습니다.
+ *
+ * @param file 현재 파일의 이름
+ * @param args 로그
+ */
 const log = (file: string, ...args: any[]) => {
   console.log(`[BJCORD][${file}]${isDev ? "[DEV]" : ""}`, ...args);
 };
 
+/**
+ * 오류 로그를 출력합니다. 브라우저의 콘솔에 출력됩니다.
+ * 개발 모드인 경우 `[DEV]` 태그가 붙습니다.
+ *
+ * @param file 현재 파일의 이름
+ * @param args 로그
+ */
 const error = (file: string, ...args: any[]) => {
   console.error(`[BJCORD][${file}]${isDev ? "[DEV]" : ""}`, ...args);
 };
 
+/**
+ * 로거 인터페이스
+ */
 export interface Logger {
   log: (...args: any[]) => void;
   error: (...args: any[]) => void;
